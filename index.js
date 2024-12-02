ef97c83e08d25f73beff65f3d60360cce3ecfbf8
Starlee Jiles <starlee.jiles@icloud.com>
2024-12-02 04:28:35 +0000

    create env and index files

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { Pool } = require('pg');
+require('dotenv').config();
+
+const app = express();
+app.use(bodyParser.json());
+
+const pool = new Pool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+});
+
+app.get('/', (req, res) => {
+    res.json({ message: 'The Joy of Painting API is running!' });
+});
